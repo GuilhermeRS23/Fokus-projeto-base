a6e170d1a748437bd03d1650eb046bae3ebac873
GuilhermeRS23 <guilhermemuralha2016@gmail.com>
2023-11-27 21:34:32 -0300

    Aula 1 - Adicionado Tarefas

---
+++ script-CRUD.js
@@ -0,0 +1,19 @@
+//Localizando os elementos
+const btnAdicionarTarefa = document.querySelector('.app__button--add-task');
+const formAdicionarTarefa = document.querySelector('.app__form-add-task');
+const textArea = document.querySelector('.app__form-textarea');
+
+const tarefas = [];
+
+btnAdicionarTarefa.addEventListener('click', () => {
+    formAdicionarTarefa.classList.toggle('hidden')
+})
+
+formAdicionarTarefa.addEventListener('submit' , (evento) => {
+    evento.preventDefault();
+    const tarefa = {
+        descricao: textArea.value
+    }
+    tarefas.push(tarefa);
+    localStorage.setItem('Tarefas', JSON.stringify(tarefas));
+})
